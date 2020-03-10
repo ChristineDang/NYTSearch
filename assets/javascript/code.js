1dd75552a2da8662b7dbe64467fea821b982fcd5
Hunter Lintz <hunterlintz126@gmail.com>
2020-03-09 20:56:16 -0400

    Added code to get basic response info

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,6 +1,11 @@
 var APIKey = "ciyO7EAKGNfSD3iDsnjOufrSOYZAG9ZC";
+var Search = "Brother";
+var beginYear = "2010";
+var bY = beginYear + "0101";
+var endYear = "2015";
+var eY = endYear + "1231";
 
-var queryURL = "https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key="+ APIKey;
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?facet_fields=pub_year&facet=true&begin_date="+bY+"&end_date="+eY+"&q="+Search+"&api-key="+APIKey;
 
 $.ajax({
   url:queryURL,
@@ -9,4 +14,9 @@ $.ajax({
 
   .then(function(response){
     console.log(response);
+    for(var i = 0; i < 5; i++){
+      console.log(response.response.docs[i].headline.main);
+      console.log(response.response.docs[i].web_url);
+      console.log(response.response.docs[i].byline.original);
+    }
   });
